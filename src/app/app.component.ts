e501e68b32799664ddca891aee4a622e5aeabfb4
Sai Krishna <w0845777@myscc.ca>
2024-09-11 14:32:15 -0400

    Created starter Angular file and completed Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'SaiKrishnaLearningAngular';
+  firstName: String = "SaiKrishna";
+  LastName: String = "Suresh";
+  age: Number = 24;
 }
