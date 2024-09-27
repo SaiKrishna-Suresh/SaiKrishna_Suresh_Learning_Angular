a2b0fe09ad42881375f2a31966f5880ede6396b3
Sai Krishna Suresh <w0845777@myscc.ca>
2024-09-27 19:05:48 -0400

    Added playerList and playerListItem

---
+++ src/app/players-list/content-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-players-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-list.component.html',
+  styleUrl: './content-list.component.css'
+})
+export class ContentListComponent {
+
+}
