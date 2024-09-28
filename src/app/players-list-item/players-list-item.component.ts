d0cc95fd0f71793f009beda73562df3a4125db90
Sai Krishna Suresh <w0845777@myscc.ca>
2024-09-27 20:25:30 -0400

    Ng for added

--- src/app/players-list-item/players-list-item.component.ts
+++ src/app/players-list-item/players-list-item.component.ts
@@ -1,10 +1,11 @@
 import {Component, input, Input} from '@angular/core';
 import {Players} from "../Shared/Models/Players";
+import { NgIf, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-players-list-item',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgForOf],
   templateUrl: './players-list-item.component.html',
   styleUrl: './players-list-item.component.css'
 })
