12bd622e54d73e18021f3018c41af87ebe5e91bf
Sai Krishna Suresh <w0845777@myscc.ca>
2024-10-08 13:05:00 -0400

    part B completed

---
+++ src/app/Data/mock-content.ts
@@ -0,0 +1,26 @@
+import {Players} from "../Shared/Models/Players";
+
+export const MOCK_CONTENT: Players[] = [
+  {
+    rank: 1,
+    firstName: 'Virat',
+    lastName: 'Kohli',
+    country: 'India',
+    average: 55.5,
+  },
+  {
+    rank: 2,
+    firstName: 'Steve',
+    lastName: 'Smith',
+    country: 'Australia',
+    average:45.5,
+  },
+  {
+    rank:3,
+    firstName:'Kane',
+    lastName:'Willaimson',
+    country:'New Zealand',
+    average:44.3,
+  },
+
+];
