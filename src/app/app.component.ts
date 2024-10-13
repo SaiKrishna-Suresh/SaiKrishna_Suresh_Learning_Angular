5bf82628d333a0e770d769227f287aaf846bb993
Saik Krishna Suresh <w0845777@myscc.ca>
2024-10-13 19:36:52 -0400

    part E completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Players} from "./Shared/Models/Players";
 import { NgForOf } from '@angular/common';
 import {PlayersListComponent} from "./players-list/players-list.component";
+import {CricketPlayerService} from "./Services/cricket-player.service";
+import {PlayersListItemComponent} from "./players-list-item/players-list-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, PlayersListComponent],
+  imports: [RouterOutlet, NgForOf, PlayersListComponent, PlayersListItemComponent],
   templateUrl: './app.component.html',
   styleUrls:[ './app.component.css']
 })
 
-export class AppComponent {
-  title = 'Assignment2';
-  player1:Players []=[{rank:1,firstName:"Virat",lastName:"kohli",country:"India",average:55},
-    {rank:2,firstName:"Steve",lastName:"Smith",country:"Australia",average:49},
-    {rank:3,firstName:"Rohit",lastName:"Sharma",country:"India",average:45},
-    {rank:4,firstName:"Joe Benjamin",lastName:"Root",country:"England",average:44.3},
-    {rank:5,firstName:"Kane",lastName:"Williamson",country:"New Zealand",average:42.2}];
-
+export class AppComponent implements OnInit{
+  player1?:Players;
+  constructor(private cricketPlayerService: CricketPlayerService) {}
+  ngOnInit():void{
+    this.getPlayerById(1);
+  }
+  getPlayerById(id:number): void{
+    this.cricketPlayerService.getPlayerById(id).subscribe((player)=>{
+      this.player1 = player;
+});
+}
 }
