2bd1cca6f065dfd1b73c078b2e322cc94f0c686c
Saik Krishna Suresh <w0845777@myscc.ca>
2024-10-13 20:58:38 -0400

    part C completed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {PlayersListComponent} from "./app/players-list/players-list.component";
+import {PlayersListItemComponent} from "./app/players-list-item/players-list-item.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes=[
+  {path:'',component:PlayersListComponent},
+  {path: 'players',component: PlayersListComponent},
+  {path:'players/:id',component:PlayersListItemComponent},
+  {path: 'modify', component:ModifyListItemComponent},
+  {path: '**', component:PageNotFoundComponent},
+];
+bootstrapApplication(AppComponent,{
+  providers:[provideRouter(routes)]
+
+}).then(r=>console.log('Bootstrap Successful'));
