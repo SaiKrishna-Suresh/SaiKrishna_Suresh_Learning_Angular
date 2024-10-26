336266fedac36061b9d1e01893b261a366a35219
Saik Krishna Suresh <w0845777@myscc.ca>
2024-10-26 14:57:03 -0400

    part B completed

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {CricketPlayerService} from "../Services/cricket-player.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-modify-list-item',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './modify-list-item.component.html',
   styleUrl: './modify-list-item.component.css'
 })
-export class ModifyListItemComponent {
+export class ModifyListItemComponent implements OnInit{
+  playerForm: FormGroup;
+  playerId?: number;
+
+  constructor(
+    private fb:FormBuilder,
+    private cricketPlayerService: CricketPlayerService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.playerForm=this.fb.group({
+      rank:['',Validators.required],
+      firstName:['',Validators.required],
+      lastName:['',Validators.required],
+      country:['',Validators.required],
+      average:['']
+    });
+
+  }
+  ngOnInit(): void {
+    this.playerId= Number(this.route.snapshot.paramMap.get('id'));
+    if(this.playerId){
+      this.cricketPlayerService.getPlayerById(this.playerId).subscribe(player=>{
+        if(player){
+          this.playerForm.patchValue(player);
+        }
+      });
+    }
+  }
+  onSubmit(): void{
+    if(this.playerId){
+      this.cricketPlayerService.updatePlayer({...this.playerForm.value, rank:this.playerId}).subscribe(()=>{
+        this.router.navigate(['/players-list']);
+      });
+    }
+  }
 
 }
