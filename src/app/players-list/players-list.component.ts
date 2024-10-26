bd91954587b13572ea7b2402f9d63cedd61f945d
Saik Krishna Suresh <w0845777@myscc.ca>
2024-10-26 15:16:58 -0400

    delete completed

--- src/app/players-list/players-list.component.ts
+++ src/app/players-list/players-list.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {Players} from "../Shared/Models/Players";
-import {PlayersListItemComponent} from "../players-list-item/players-list-item.component";
-import {NgIf, NgForOf, NgClass} from '@angular/common';
-import {CricketPlayerService} from "../Services/cricket-player.service";
+import { Component, OnInit } from '@angular/core';
+import { Players } from "../Shared/Models/Players";
+import { PlayersListItemComponent } from "../players-list-item/players-list-item.component";
+import { NgIf, NgForOf, NgClass } from '@angular/common';
+import { CricketPlayerService } from "../Services/cricket-player.service";
+import { Router } from '@angular/router'; // Import Router for navigation
+
 @Component({
   selector: 'app-players-list',
   standalone: true,
@@ -10,45 +12,40 @@ import {CricketPlayerService} from "../Services/cricket-player.service";
     PlayersListItemComponent, NgIf, NgForOf, NgClass,
   ],
   templateUrl: './players-list.component.html',
-  styleUrl: './players-list.component.css'
+  styleUrls: ['./players-list.component.css'] // Fixed to styleUrls
 })
 export class PlayersListComponent implements OnInit {
   players: Players[] = [];
-  selectedPlayer?:Players;
 
-  constructor(private cricketPlayerService: CricketPlayerService) {}
-    ngOnInit():void {
-      this.loadAllPlayers();
-    }
-    loadAllPlayers():void{
-    this.cricketPlayerService.getAllPlayers().subscribe((data:Players[])=>{
-      this.players=data;
-      });
-    }
-    addPlayer(newPlayer:Players):void{
-    this.cricketPlayerService.addPlayer(newPlayer).subscribe((updatedPlayers: Players[])=>{
+  constructor(
+    private cricketPlayerService: CricketPlayerService,
+    private router: Router // Inject Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadAllPlayers();
+  }
+
+  loadAllPlayers(): void {
+    this.cricketPlayerService.getAllPlayers().subscribe((data: Players[]) => {
+      this.players = data;
+    });
+  }
+
+  addPlayer(newPlayer: Players): void {
+    this.cricketPlayerService.addPlayer(newPlayer).subscribe((updatedPlayers: Players[]) => {
       this.players = updatedPlayers;
     });
-    }
-    updatePlayer(updatedPlayer:Players):void{
-    this.cricketPlayerService.updatePlayer(updatedPlayer).subscribe((updatedPlayer: Players[])=>{
-      this.players= updatedPlayer;
-      });
-    }
-    deletePlayer(id: number):void{
-    this.cricketPlayerService.deletePlayer(id).subscribe((removedPlayer)=>{
-      if(removedPlayer){
-        this.loadAllPlayers();
-      }
-    });
-    }
-    getPlayerById(id: number):void{
-    this.cricketPlayerService.getPlayerById(id).subscribe((player)=>{
-      if(player){
-        this.selectedPlayer=player;
-      }
-    });
-    }
+  }
 
+  editPlayer(player: Players): void {
+    this.router.navigate(['/modify-list-item', player.rank]); // Use the appropriate route
+  }
 
+  deletePlayer(id: number): void {
+    this.cricketPlayerService.deletePlayer(id).subscribe(() => {
+      this.loadAllPlayers(); // Refresh the list after deletion
+    });
+  }
 }
+
