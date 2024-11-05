2c66b47ab5c2f633a3c1bb563b46b5e8ab741d12
Saik Krishna Suresh <w0845777@myscc.ca>
2024-11-05 13:20:52 -0500

    delete completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import {Component, OnInit} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet, Routes} from '@angular/router';
 import {Players} from "./Shared/Models/Players";
 import { NgForOf } from '@angular/common';
 import {PlayersListComponent} from "./players-list/players-list.component";
 import {CricketPlayerService} from "./Services/cricket-player.service";
 import {PlayersListItemComponent} from "./players-list-item/players-list-item.component";
+import { ModifyListItemComponent } from './modify-list-item/modify-list-item.component'
 
+const routes: Routes = [
+  { path: 'players-list', component: PlayersListComponent },
+  { path: 'modify-list-item/:id', component: ModifyListItemComponent },
+  { path: '', redirectTo: 'players-list', pathMatch: 'full' } // Default route
+];
 @Component({
   selector: 'app-root',
   standalone: true,
