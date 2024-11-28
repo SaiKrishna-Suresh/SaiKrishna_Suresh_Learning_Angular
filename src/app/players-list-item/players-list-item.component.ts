f4de1c79242808fef9549389b4d5468dc0fe8a1d
SaiKrishna Suresh <w0845777@myscc.ca>
2024-11-28 13:25:28 -0500

    done

--- src/app/players-list-item/players-list-item.component.ts
+++ src/app/players-list-item/players-list-item.component.ts
@@ -1,11 +1,11 @@
 import {Component,  Input} from '@angular/core';
 import {Players} from "../Shared/Models/Players";
-import {NgIf, NgForOf, NgClass, NgOptimizedImage} from '@angular/common';
+import {NgIf, NgForOf, NgClass, NgOptimizedImage, CurrencyPipe, DatePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-players-list-item',
   standalone: true,
-  imports: [NgIf, NgClass, NgOptimizedImage],
+  imports: [NgIf, NgClass, NgOptimizedImage, CurrencyPipe, DatePipe, UpperCasePipe],
   templateUrl: './players-list-item.component.html',
   styleUrls:[ './players-list-item.component.css']
 })
